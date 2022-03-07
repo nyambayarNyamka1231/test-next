f7cd5aa6dbf3a8fcaf983d7b614b4782574a0d9f
nyambayar1231 <nyambayarnyamka1231@gmail.com>
2022-03-07 12:19:54 +0800

    h1 tailbar nemev

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-import Player from '../components/Player'
-import PlayerCSS from '../components/PlayerCss'
+import Player from "../components/Player";
+import PlayerCSS from "../components/PlayerCss";
 
 const Index = () => {
   const videoJsOptions = {
-    techOrder: ['youtube'],
+    techOrder: ["youtube"],
     autoplay: false,
     controls: true,
     sources: [
       {
-        src: 'https://www.youtube.com/watch?v=IxQB14xVas0',
-        type: 'video/youtube',
+        src: "https://www.youtube.com/watch?v=IxQB14xVas0",
+        type: "video/youtube",
       },
     ],
-  }
+  };
 
   return (
     <>
       <Player {...videoJsOptions} />
+      <h1>my next js App</h1>
       <PlayerCSS />
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
